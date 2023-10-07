f27b23de99ff9cca7b98ccbd6e4ebd26d4d102f5
Muhammad Uzair <uzairuzair050@gmail.com>
2023-10-07 19:40:41 +0500

    Created caculator class with add & subtract function only

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    // Function to add two numbers
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    // Function to subtract two numbers
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+}
